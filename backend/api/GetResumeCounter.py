c7ab098e6aa667e1f9658bde4af355f9dbf85039
Promyse Ferguson <promyseferguson@MacBook-Pro.local>
2024-05-31 18:36:02 -0400

    azure function updated

--- backend/api/GetResumeCounter.py
+++ backend/api/GetResumeCounter.py
@@ -1,28 +1,32 @@
 import azure.functions as func
 import logging
+from Counter import IdCount
+from json import dumps
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="GetResumeCounter")
 def GetResumeCounter(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    # Connect to the Cosmos DB
     cosmos_client = cosmos_client.from_connection_string("AzureRConnectionString")
     database = cosmos_client.get_database_client("Resume-db")
+    container = database.get_container_client("Counter")
+
+    # Query the Cosmos DB for the counter
+    for item in container.query_items(query='SELECT * FROM c WHERE c.id = "counter"',
+        enable_cross_partition_query=True):
+        counter = IdCount(item['id'], item['count'])
     
+    if counter is None:
+        counter = IdCount('counter', 0)
+        container.create_item(body=counter.__dict__)
+    
+    counter.count += 1
+    
+    container.replace_item(item=counter.__dict__, body=counter.__dict__)
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    return func.HttpResponse(json.dumps(counter.__dict__), mimetype="application/json", status_code=200)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+
+        
