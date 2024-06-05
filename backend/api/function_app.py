d8ebb6902f3476b8043551db759766d18b3e046f
Promyse Ferguson <promyseferguson@MacBook-Pro.local>
2024-06-05 08:18:09 -0400

    setting up things locally

--- backend/api/function_app.py
+++ backend/api/function_app.py
@@ -1,5 +1,8 @@
 import azure.functions as func
 import logging
+from azure.cosmos import CosmosClient
+import json
+import 
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -7,19 +10,20 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 def GetResumeCounter(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    Client = CosmosClient.from_connection_string("AzureResumeConnectionStr")
+    database = Client.get_database_client("Resume-db")
+    container = database.get_container_client("Counter")
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    req_body = req.get_json()
+    item_id = req_body.get('id')
+
+    try:
+        item = container.read_item(item=item_id)
+        item['counter'] += 1
+        container.replace_item(item=item_id, body=item)
+
+        return func.HttpResponse( body="Item updated successfully.", status_code=20)
+    except Exception as e:
+        logging.error(f"Error updating item: {e} ")
+        return func.HttpResponse("Error updating item.", status_code=500)
+    
