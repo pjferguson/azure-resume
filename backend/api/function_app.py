8896042659102109590e5b3e2086f69a05f07cfe
Promyse Ferguson <promyseferguson@MacBook-Pro.local>
2024-06-06 07:05:24 -0400

    making progress with backend api

--- backend/api/function_app.py
+++ backend/api/function_app.py
@@ -2,28 +2,26 @@ import azure.functions as func
 import logging
 from azure.cosmos import CosmosClient
 import json
-import 
-
+import os
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="GetResumeCounter")
 def GetResumeCounter(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    Client = CosmosClient.from_connection_string("AzureResumeConnectionStr")
+    connection_str = os.environ["AzureResumeConnectionStr"]
+    Client = CosmosClient.from_connection_string(connection_str)
     database = Client.get_database_client("Resume-db")
     container = database.get_container_client("Counter")
 
-    req_body = req.get_json()
-    item_id = req_body.get('id')
+    item_id = "1"
 
     try:
-        item = container.read_item(item=item_id)
+        item = container.read_item(item=item_id, partition_key="1")
         item['counter'] += 1
         container.replace_item(item=item_id, body=item)
 
-        return func.HttpResponse( body="Item updated successfully.", status_code=20)
+        return func.HttpResponse(body=json.dumps({"message": "Item updated successfully.", "counter": item["counter"]}), status_code=200, mimetype="application/json")
     except Exception as e:
         logging.error(f"Error updating item: {e} ")
-        return func.HttpResponse("Error updating item.", status_code=500)
+        return func.HttpResponse(body=json.dumps({"error":"Error updating item."}), status_code=500, mimetype="application/json")
     
